aeabe3184f1f59ed12092b5d4d7cdb39a69f898a
Krzysztof Zaucha <krzysztof@zaucha.co.uk>
2025-04-01 19:44:43 +0100

    Add project files

--- main.go
+++ main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+
+	"github.com/codemity/gotpl/internal/app"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	application := app.New(
+		app.WithValues(
+			name,
+			``,
+			version,
+			copyright,
+			authorName,
+			authorEmail,
+			buildTime,
+		),
+	)
+
+	application.Commands = []*cli.Command{}
+
+	if e := application.Run(os.Args); e != nil {
+		log.Fatalf("error occurred during execution")
+	}
+}
