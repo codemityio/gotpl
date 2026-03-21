1d143323efe9f9a2c7cd9de5572a7edf012ffe3a
Krzysztof Zaucha <krzysztof@zaucha.co.uk>
2026-03-21 23:42:47 +0000

    tools and docs correction

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -187,7 +187,7 @@ EOF
 "install")
   go install -ldflags "\
     -X 'main.name=${BASE_NAME}' \
--X 'main.version=$(scripts/tools.sh version)' \
+-X 'main.version=latest' \
 -X 'main.copyright=${VENDOR}' \
 -X 'main.authorName=${VENDOR}' \
 -X 'main.buildTime=$(date -u +"%Y-%m-%dT%H:%M:%SZ")'\
